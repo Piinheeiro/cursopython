aac5d85ee43ff67e253149ebebbede6ec75387c9
Piinheeiro <moisesmaps@gmail.com>
2025-08-15 16:19:20 -0300

    Mensagem do commit

--- calculadora-imc.py
+++ calculadora-imc.py
@@ -2,4 +2,7 @@ peso = float(input("Digite seu peso em kg: "))
 altura = float(input("Digite sua altura em metros: "))  
 imc = peso / (altura ** 2)
 
+if imc < 18.5:
+    print("Você está abaixo do peso.")
 print(f"Seu IMC é: {imc:.2f}")
+ 

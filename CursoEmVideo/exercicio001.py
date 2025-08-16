2da1c2f1db7164367e6dfd796edca14faeef2e24
Piinheeiro <moisesmaps@gmail.com>
2025-08-16 10:12:48 -0300

    mensagem aleatoria

---
+++ CursoEmVideo/exercicio001.py
@@ -0,0 +1,5 @@
+nome = input ('qual é o seu nome?')
+idade = input ('quantos anos voce tem?')
+peso = input('qual o seu peso?')
+
+print('o nome do cliente é ', nome, ' tem ', idade,' e pesa ', peso)

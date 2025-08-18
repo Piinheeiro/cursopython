77a57613c050b33684fbbfc1fe92cd7f8b6ceb79
piinheeiro <moisesmaps@gmail.com>
2025-08-17 22:54:32 -0300

    exercicio aula 001

---
+++ custo_viagem.py
@@ -0,0 +1,6 @@
+km = float (input( "quantidade de quilometros."))
+consumo = float (input("qual o consumo/litro?"))
+preco = float (input("qual o valor da gasolina?"))
+
+
+print ("voce precisará abastecer um valor total de R$", (km/consumo)*preco)

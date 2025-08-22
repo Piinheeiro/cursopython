148b406f4152890fc6e003c3671fae7a4c73b14c
piinheeiro <moisesmaps@gmail.com>
2025-08-21 21:01:41 -0300

    informações iniciais

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,9 @@
-n1 = float (input("Digite o primeiro numero? "))
-op = input ("Digite a operação ( + , - , * , /)")
-n2 = float (input("Digite o segundo número? "))
+# aula 21/08/25
+# faça uma calculador que some dois números, informe o operador e apresente o resultado.
 
+n1 = float (input("Digite o primeiro numero? "))
+n2 = float (input("Digite o segundo número? "))
+op = input ("Digite a operação ( + , - , * , /)")
 
 
 match op:

985dcd39e8a5f19c2d5c5dbd2765be450818f0cf
piinheeiro <moisesmaps@gmail.com>
2025-08-24 11:45:57 -0300

    teste

---
+++ inventario.py
@@ -0,0 +1,6 @@
+inventario = {"arroz": "400","feijao":"100"}
+
+print = ("=====  O QUE DESEJA FAZER?  =====") 
+print = ("[1] - ADICIONAR \n[2] - REMOVER \n[3] - LISTAR \n[4] - SAIR")
+
+#opcao = input ("Selecionar: ")
